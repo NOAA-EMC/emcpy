8c36c5d2be7cd0d73a84790342a438634ec600ca
Rahul Mahajan <aerorahul@users.noreply.github.com>
2021-07-22 14:40:57 -0400

    fix errors in setup.py

--- setup.py
+++ setup.py
@@ -6,25 +6,25 @@ script_dir = 'src/emcpy/bin'
 scripts = [os.path.join(script_dir, x) for x in os.listdir(script_dir)]
 
 setup(
-    name = 'emcpy',
-    author = 'NOAA-EMC',
-    version = '0.0.1',
-    description = 'A collection of python tools used at EMC',
-    url = 'https://github.com/noaa-emc/emcpy',
-    keywords = 'NOAA, EMC',
-    package_dir = {'':'src'},
-    packages = find_packages(where='src'),
-    package_data = {
+    name='emcpy',
+    author='NOAA-EMC',
+    version='0.0.1',
+    description='A collection of python tools used at EMC',
+    url='https://github.com/noaa-emc/emcpy',
+    keywords='NOAA, EMC',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    package_data={
         '': ['cfg/*'],
-            },
-    include_package_data = True,
-    scripts = scripts,
-    python_requires = '>= 3.6',
-    install_requires = [
+    },
+    include_package_data=True,
+    scripts=scripts,
+    python_requires='>= 3.6',
+    install_requires=[
         'pyyaml',
         'click',
     ],
-    classifiers = [
+    classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
