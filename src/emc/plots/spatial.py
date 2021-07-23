af2fbf34384be172714ea633b3a24c2be5ea3a0c
Kevin Dougherty <kevin.dougherty@noaa.gov>
2021-07-23 15:45:27 +0000

    updates to address comments; split map projections into functions; created functions that plot 1D/2D data

--- src/emc/plots/spatial.py
+++ src/emc/plots/spatial.py
@@ -1,3 +1,5 @@
+__all__ = ['spatial']
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
@@ -8,9 +10,9 @@ import matplotlib.colors as mcolors
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 
 
-def _global(latitude, longitude, data, **kwargs):
+def _global():
     """
-    Plot data on global domain.
+    Return fig, ax on global domain.
     """
     fig = plt.figure(figsize=(12, 10))
     ax = fig.add_subplot(1, 1, 1,
@@ -24,52 +26,12 @@ def _global(latitude, longitude, data, **kwargs):
     ax.xaxis.set_major_formatter(lon_formatter)
     ax.yaxis.set_major_formatter(lat_formatter)
 
-    cs = plt.scatter(longitude, latitude, c=data, s=10,
-                     vmin=kwargs['vmin'], vmax=kwargs['vmax'],
-                     cmap=kwargs['cmap'], transform=ccrs.PlateCarree())
-
-    cb = plt.colorbar(cs, shrink=0.5, pad=.03, extend='both')
-    cb.set_label(kwargs['cbar_label'])
-
-    plt.title(kwargs['title'], loc='left')
-    plt.title(kwargs['cycle'], loc='right', fontweight='semibold')
-
-    return fig
-
-
-def _conus(latitude, longitude, data, **kwargs):
-    """
-    Plot data on CONUS domain.
-    """
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(1, 1, 1,
-                         projection=ccrs.PlateCarree(central_longitude=0))
-    ax.add_feature(cfeature.GSHHSFeature(scale='auto'))
-    ax.set_extent([-130, -60, 10, 60])
-    ax.set_xticks([-130, -120, -110, -100, -90, -80, -70, -60],
-                  crs=ccrs.PlateCarree())
-    ax.set_yticks([10, 20, 30, 40, 50, 60], crs=ccrs.PlateCarree())
-    lon_formatter = LongitudeFormatter(zero_direction_label=False)
-    lat_formatter = LatitudeFormatter()
-    ax.xaxis.set_major_formatter(lon_formatter)
-    ax.yaxis.set_major_formatter(lat_formatter)
-
-    cs = plt.scatter(longitude, latitude, c=data, s=10,
-                     vmin=kwargs['vmin'], vmax=kwargs['vmax'],
-                     cmap=kwargs['cmap'], transform=ccrs.PlateCarree())
-
-    cb = plt.colorbar(cs, shrink=0.5, pad=.03, extend='both')
-    cb.set_label(kwargs['cbar_label'])
-
-    plt.title(kwargs['title'], loc='left')
-    plt.title(kwargs['cycle'], loc='right', fontweight='semibold')
-
-    return fig
+    return fig, ax
 
 
 def _north_america():
     """
-    Plot data on CONUS domain.
+    Return fig, ax on CONUS domain.
     """
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(1, 1, 1,
@@ -84,27 +46,74 @@ def _north_america():
     ax.xaxis.set_major_formatter(lon_formatter)
     ax.yaxis.set_major_formatter(lat_formatter)
 
-    cs = plt.scatter(longitude, latitude, c=data, s=10,
-                     vmin=kwargs['vmin'], vmax=kwargs['vmax'],
-                     cmap=kwargs['cmap'], transform=ccrs.PlateCarree())
+    return fig, ax
+
+
+def _conus():
+    """
+    Return fig, ax on CONUS domain.
+    """
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(1, 1, 1,
+                         projection=ccrs.PlateCarree(central_longitude=0))
+    ax.add_feature(cfeature.GSHHSFeature(scale='auto'))
+    ax.set_extent([-130, -60, 10, 60])
+    ax.set_xticks([-130, -120, -110, -100, -90, -80, -70, -60],
+                  crs=ccrs.PlateCarree())
+    ax.set_yticks([10, 20, 30, 40, 50, 60], crs=ccrs.PlateCarree())
+    lon_formatter = LongitudeFormatter(zero_direction_label=False)
+    lat_formatter = LatitudeFormatter()
+    ax.xaxis.set_major_formatter(lon_formatter)
+    ax.yaxis.set_major_formatter(lat_formatter)
+
+    return fig, ax
+
+
+def _spatial_scatter(fig, ax, longitude, latitude, data, plotopts):
+    """
+    Plot 1-dimensional data on a map as scatter.
+    """
+    cs = plt.scatter(longitude, latitude, c=data, s=plotopts['marker size'],
+                     vmin=plotopts['vmin'], vmax=plotopts['vmax'],
+                     cmap=plotopts['cmap'], transform=ccrs.PlateCarree())
 
     cb = plt.colorbar(cs, shrink=0.5, pad=.03, extend='both')
-    cb.set_label(kwargs['cbar_label'])
+    cb.set_label(plotopts['cbar label'])
 
-    plt.title(kwargs['title'], loc='left')
-    plt.title(kwargs['cycle'], loc='right', fontweight='semibold')
+    plt.grid(plotopts['grid'])
+    plt.title(plotopts['title'], loc='left')
+    plt.title(plotopts['cycle'], loc='right', fontweight='semibold')
+
+    return fig
+
+
+def _spatial_pcolormesh(fig, ax, longitude, latitude, data, plotopts):
+    """
+    Plot 2-dimensional data on a map as pcolormesh.
+    """
+    cs = plt.pcolormesh(lon, lat, data, cmap=plotopts['cmap'],
+                        vmin=plotopts['vmin'], vmax=plotopts['vmax'],
+                        transform=ccrs.PlateCarree())
+
+    cb = plt.colorbar(cs, shrink=0.5, pad=.03, extend='both')
+    cb.set_label(plotopts['cbar label'])
+
+    plt.grid(plotopts['grid'])
+    plt.title(plotopts['title'], loc='left')
+    plt.title(plotopts['cycle'], loc='right', fontweight='semibold')
 
     return fig
 
 
 def spatial(latitude, longitude, data, domain='global', cmap='viridis',
-            vmin=None, vmin=None, title=None, cycle=None, cbar_label=None):
+            markersize=10, grid=False, vmin=None, vmax=None, title=None,
+            cycle=None, cbar_label=None):
     """
     Plot a spatial map of desired data.
     Paramaters
     ----------
     Required:
-    latitude, longitude, data : array type
+    latitude, longitude, data : array type (1D or 2D)
     domain : domain of map (str)
 
     Optional:
@@ -121,16 +130,31 @@ def spatial(latitude, longitude, data, domain='global', cmap='viridis',
     domain = domain.lower()
 
     # Dictionary of kwargs
-    kwargs = {'cmap': cmap, 'vmin': vmin, 'vmax': vmax,
-              'title': title, 'cycle': cycle, 'cbar_label': cbar_label}
+    plotopts = {'cmap': cmap, 'marker size': markersize, 'vmin': vmin,
+                'vmax': vmax, 'grid': grid, 'title': title, 'cycle': cycle,
+                'cbar label': cbar_label}
 
+    # Get domain fig/ax
     if domain == 'global':
-        fig = _global(latitude, longitude, data, **kwargs)
+        fig, ax = _global()
 
     elif domain == 'conus':
-        fig = _conus(latitude, longitude, data, **kwargs)
+        fig, ax = _conus()
 
     elif domain == 'north america':
-        fig = _north_america(latitude, longitude, data, **kwargs)
+        fig, ax = _north_america()
+
+    else:
+        raise TypeError(f'{domain} is not a valid domain. Current ' +
+                        'domains supported are: global, conus, north america')
+
+    # Plot scatter for 1D data; pcolormesh for 2D data
+    if len(data.shape) == 1:
+        fig = _spatial_scatter(fig, ax, latitude, longitude, data, plotopts)
+    elif len(data.shape) == 2:
+        fig = _spatial_pcolormesh(fig, ax, latitude, longitude, data, plotopts)
+    else:
+        raise TypeError('Data is not 1D or 2D. Please enter correct ' +
+                        'dimension data.')
 
     return fig
