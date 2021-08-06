0c5f13a4a47ca211ca83e4af486ede52593b5bbd
Kevin Dougherty <kevin.dougherty@noaa.gov>
2021-08-06 19:43:10 +0000

    addressing Rahul's suggestions

--- src/emcpy/plots/map_tools.py
+++ src/emcpy/plots/map_tools.py
@@ -3,7 +3,7 @@ import cartopy.crs as ccrs
 
 class Domain:
 
-    def __init__(self, domain='global'):
+    def __init__(self, domain='global', dd=dict()):
 
         domain = domain.lower()
 
@@ -13,50 +13,61 @@ class Domain:
             "north america": self._north_america,
             "europe": self._europe}
 
-        map_domains[domain]()
-
         try:
-            return map_domains[domain]()
+            map_domains[domain](dd=dd)
         except KeyError:
             raise TypeError(f'{domain} is not a valid domain.' +
                             'Current domains supported are:\n' +
                             f'{" | ".join(map_domains.keys())}"')
 
-    def _global(self):
+    def _global(self, dd=dict()):
         """
         Sets extent, longitude xticks, and latitude yticks
         for a global domain.
         """
         self.extent = (-180, 180, -90, 90)
-        self.xticks = (-180, -120, -60, 0, 60, 120, 180)
-        self.yticks = (-90, -60, -30, 0, 30, 60, 90)
+        self.xticks = dd.get('xticks', (-180, -120, -60,
+                                        0, 60, 120, 180))
+        self.yticks = dd.get('yticks', (-90, -60, -30, 0,
+                                        30, 60, 90))
 
-    def _north_america(self):
+    def _north_america(self, dd=dict()):
         """
         Sets extent, longitude xticks, and latitude yticks
         for a north american domain.
         """
         self.extent = (-170, -50, 7.5, 75)
-        self.xticks = (-170, -150, -130, -110, -90, -70, -50)
-        self.yticks = (10, 30, 50, 70)
+        self.xticks = dd.get('xticks', (-170, -150, -130, -110,
+                                        -90, -70, -50))
+        self.yticks = dd.get('yticks', (10, 30, 50, 70))
 
-    def _conus(self):
+    def _conus(self, dd=dict()):
         """
         Sets extent, longitude xticks, and latitude yticks
         for a contiguous United States domain.
         """
         self.extent = (-130, -65, 20, 55)
-        self.xticks = (-130, -120, -110, -100, -90, -80, -70, -60)
-        self.yticks = (25, 35, 45, 55)
+        self.xticks = dd.get('xticks', (-130, -120, -110, -100,
+                                        -90, -80, -70, -60))
+        self.yticks = dd.get('yticks', (25, 35, 45, 55))
 
-    def _europe(self):
+    def _europe(self, dd=dict()):
         """
         Sets extent, longitude xticks, and latitude yticks
         for a European domain.
         """
         self.extent = (-12.5, 40, 30, 70)
-        self.xticks = (-10, 0, 10, 20, 30, 40)
-        self.yticks = (30, 40, 50, 60, 70)
+        self.xticks = dd.get('xticks', (-10, 0, 10, 20, 30, 40))
+        self.yticks = dd.get('yticks', (30, 40, 50, 60, 70))
+        
+    def _custom(self, dd=dict()):
+        """
+        Sets extent, longitude xticks, and latitude yticks
+        for a Custom domain.
+        """
+        self.extent = dd.extent
+        self.xticks = dd.xticks
+        self.yticks = dd.yticks
 
 
 class MapProjection:
