2fd8f382e08fae2fbb0662d2bfa6ce052f7fdfeb
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2021-08-25 17:37:00 +0000

    Format test prints

--- src/tests/test_defaults.py
+++ src/tests/test_defaults.py
@@ -4,10 +4,13 @@ from emcpy.plots import VariableSpecs
 def test_defaults_gdas_variable_specs():
     # read YAML file stored in cfg/
     # print some output
+    print('--- Reading Temperature Defaults For Magnitude ---')
     # temperature
     varspecs = VariableSpecs(variable='temperature',
                              eval_type='magnitude')
-    print(varspecs.name)
-    print(varspecs.sname)
-    print(varspecs.type)
-    print(varspecs.range)
+    print(f'Name:          {varspecs.name}')
+    print(f'Short Name:    {varspecs.sname}')
+    print(f'Variable Type: {varspecs.type}')
+    print(f'Colormap:      {varspecs.cmap}')
+    print(f'VMin:          {varspecs.vmin}')
+    print(f'VMax:          {varspecs.vmax}')
