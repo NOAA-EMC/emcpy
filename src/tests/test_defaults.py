3ad7131272bf009e0f9a1429bf7057ec4a49efeb
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2021-08-25 17:26:31 +0000

    Add test_defaults

---
+++ src/tests/test_defaults.py
@@ -0,0 +1,10 @@
+from emcpy.plots import VariableSpecs
+
+
+def test_defaults_gdas_variable_specs():
+    # read YAML file stored in cfg/
+    # print some output
+    # temperature
+    varspecs = VariableSpecs(variable='temperature',
+                             eval_type='magnitude')
+    print(varspecs)
