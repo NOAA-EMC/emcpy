3e86e972bf615f061abafcd44fbbbfc8407f608d
Kevin Dougherty <kevin.dougherty@noaa.gov>
2021-10-22 19:02:14 +0000

    add histogram examples

---
+++ src/examples/histograms/histogram.py
@@ -0,0 +1,25 @@
+import numpy as np
+from emcpy.plots.plots import Histogram
+from emcpy.plots.create_plots import CreatePlot
+
+# Generate test data for histogram plots
+mu = 100  # mean of distribution
+sigma = 15  # standard deviation of distribution
+data = mu + sigma * np.random.randn(437)
+
+# Create histogram object
+hst = Histogram(data)
+hst.color = 'tab:green'
+hst.alpha = 0.7
+hst.label = 'data'
+
+# Create histogram plot and draw data
+myplt = CreatePlot()
+plt_list = [hst]
+myplt.draw_data(plt_list)
+
+# Add features
+myplt.add_title(label='Test Histogram Plot')
+myplt.add_xlabel(xlabel='X Axis Label')
+myplt.add_ylabel(ylabel='Y Axis Label')
+myplt.add_legend()
---
+++ src/examples/histograms/layered_histogram.py
@@ -0,0 +1,31 @@
+import numpy as np
+from emcpy.plots.plots import Histogram
+from emcpy.plots.create_plots import CreatePlot
+
+# Generate test data for histogram plots
+mu = 100  # mean of distribution
+sigma = 15  # standard deviation of distribution
+data1 = mu + sigma * np.random.randn(450)
+data2 = mu + sigma * np.random.randn(225)
+
+# Create histogram objects
+hst1 = Histogram(data1)
+hst1.color = 'tab:green'
+hst1.alpha = 0.7
+hst1.label = 'data 1'
+
+hst2 = Histogram(data2)
+hst2.color = 'tab:purple'
+hst2.alpha = 0.7
+hst2.label = 'data 2'
+
+# Create histogram plot and draw data
+myplt = CreatePlot()
+plt_list = [hst1, hst2]
+myplt.draw_data(plt_list)
+
+# Add features
+myplt.add_title(label='Test Histogram Plot')
+myplt.add_xlabel(xlabel='X Axis Label')
+myplt.add_ylabel(ylabel='Y Axis Label')
+myplt.add_legend()
