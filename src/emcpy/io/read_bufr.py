9c753c71b3bd040223f0a8877a6e5797bb16b1e4
Kevin Dougherty <kevin.dougherty@noaa.gov>
2021-12-20 18:39:24 +0000

    pycodestyle

--- src/emcpy/io/read_bufr.py
+++ src/emcpy/io/read_bufr.py
@@ -5,18 +5,19 @@ import yaml
 import os
 from pathlib import Path
 
+
 class ReadBUFR:
-    
+
     def __init__(self, file):
         """
         Initialize a BUFR file object.
 
         Args:
-            path : (str) path to prep BUFR file
+            file : (str) path to prep BUFR file
         """
-        
-        self.file = file       
-    
+
+        self.file = file
+
     def read_radiance(self, sensor, satellite):
         """
         Open and grab all data from BUFR file that falls within sensor
@@ -30,7 +31,7 @@ class ReadBUFR:
         """
         self.hdstr1 = 'SAID SIID FOVN YEAR MNTH DAYS HOUR MINU SECO CLAT CLON CLATH CLONH HOLS'
         self.hdstr2 = 'SAZA SOZA BEARAZ SOLAZI'
-        
+
         df_dict = {
             'datetime': [],
             'channel': [],
@@ -42,32 +43,32 @@ class ReadBUFR:
             'solar_azimuth': [],
             'brightness temperature': []
         }
-        
+
         _emcpy_dir = Path(__file__).parent.parent.absolute()
         var_yaml = os.path.join(_emcpy_dir,
                                 'cfg',
                                 'bufr_codes.yaml')
-        
+
         # read YAML into dictionary
         with open(var_yaml) as yamlfile:
             bufr_dict = yaml.load(yamlfile, Loader=yaml.FullLoader)
-        
+
         sensor_id = bufr_dict['sensor'][sensor]
         sat_id = bufr_dict['satellite'][satellite]
-        
-        bufr = ncepbufr.open(file)
+
+        bufr = ncepbufr.open(self.file)
         bufr.print_table()
         while bufr.advance() == 0:
             while bufr.load_subset() == 0:
                 hdr1 = bufr.read_subset(self.hdstr1).squeeze()
                 if sat_id == int(hdr1[0]) and sensor_id == int(hdr1[1]):
                     hdr2 = bufr.read_subset(self.hdstr2).squeeze()
-                    
-                    str_datetime ='%04i%02i%02i%02i%02i%02i' % tuple(hdr1[3:9])
-                    
-                    channels = bufr.read_subset('CHNM', rep=True).squeeze() # channels
-                    tb = bufr.read_subset('TMBR', rep=True).squeeze() # brightness temp
-                    
+
+                    str_datetime = '%04i%02i%02i%02i%02i%02i' % tuple(hdr1[3:9])
+
+                    channels = bufr.read_subset('CHNM', rep=True).squeeze()  # channels
+                    tb = bufr.read_subset('TMBR', rep=True).squeeze()  # brightness temp
+
                     for k, chan in enumerate(channels):
                         df_dict['datetime'].append(str_datetime)
                         df_dict['latitude'].append(hdr1[9])
@@ -78,10 +79,10 @@ class ReadBUFR:
                         df_dict['solar_azimuth'].append(hdr2[3])
                         df_dict['channel'].append(int(chan))
                         df_dict['brightness temperature'].append(tb[k])
-                    
+
         bufr.close()
-        
+
         df = pd.DataFrame(df_dict)
         df.set_index('channel', inplace=True)
-        
+
         return df
