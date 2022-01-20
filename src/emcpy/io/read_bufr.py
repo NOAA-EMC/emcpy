a41e5b116bf2e32cb05d5c3a89bfc83fe3c9a580
Kevin Dougherty <kevin.dougherty@noaa.gov>
2022-01-20 20:33:49 +0000

    updates to read_bufr.py to use yaml input

--- src/emcpy/io/read_bufr.py
+++ src/emcpy/io/read_bufr.py
@@ -4,50 +4,41 @@ import pandas as pd
 import yaml
 import os
 from pathlib import Path
+import yaml
 
 
 class ReadBUFR:
 
-    def __init__(self, file):
+    def __init__(self, input_yaml):
         """
         Initialize a BUFR file object.
 
         Args:
-            file : (str) path to prep BUFR file
+            input_yaml : (str) path to input yaml file
         """
 
-        self.file = file
+        with open(input_yaml) as yamlfile:
+            input_yaml = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
-    def read_radiance(self, sensor, satellite):
+        if 'radiance' in [*input_yaml.keys()]:
+            self.bufr_file = input_yaml['radiance']['obsdatain']
+            self.inputs = input_yaml['radiance']['inputs']
+            self.variables = input_yaml['radiance']['variables']
+
+    def read_radiance(self):
         """
-        Open and grab all data from BUFR file that falls within sensor
-        and satellite inputs.
-
-        Args:
-            sensor : (str) string of sensor
-            satellite : (str) string of satellite
-        Returns:
-            df : pandas dataframe with information
+        Open and read bufr file containing radiance data.
         """
-        self.hdstr1 = 'SAID SIID FOVN YEAR MNTH DAYS HOUR MINU SECO CLAT CLON CLATH CLONH HOLS'
-        self.hdstr2 = 'SAZA SOZA BEARAZ SOLAZI'
-
-        df_dict = {
-            'datetime': [],
-            'channel': [],
-            'latitude': [],
-            'longitude': [],
-            'satellite_zenith_angle': [],
-            'solar_zenith_angle': [],
-            'bearing_or_azimuth': [],
-            'solar_azimuth': [],
-            'brightness temperature': []
-        }
-
+        # Get BUFR codes
         _emcpy_dir = Path(__file__).parent.parent.absolute()
         var_yaml = os.path.join(_emcpy_dir,
                                 'cfg',
                                 'bufr_codes.yaml')
+
+        # Get input sensor, satellite, channels
+        sensor = self.inputs['sensor']
+        satellite = self.inputs['satellite']
+        channels = self._channel_list(self.inputs['channels'])
 
         # read YAML into dictionary
         with open(var_yaml) as yamlfile:
@@ -56,33 +47,91 @@ class ReadBUFR:
         sensor_id = bufr_dict['sensor'][sensor]
         sat_id = bufr_dict['satellite'][satellite]
 
-        bufr = ncepbufr.open(self.file)
-        bufr.print_table()
+        # Create output dictionary
+        df_dict = {}
+        for key in self.variables.keys():
+            df_dict[key] = []
+
+        # Open BUFR and store into dataframe
+        bufr = ncepbufr.open(self.bufr_file)
         while bufr.advance() == 0:
             while bufr.load_subset() == 0:
-                hdr1 = bufr.read_subset(self.hdstr1).squeeze()
-                if sat_id == int(hdr1[0]) and sensor_id == int(hdr1[1]):
-                    hdr2 = bufr.read_subset(self.hdstr2).squeeze()
+                said = bufr.read_subset('SAID', rep=True).squeeze()
+                siid = bufr.read_subset('SIID', rep=True).squeeze()
 
-                    str_datetime = '%04i%02i%02i%02i%02i%02i' % tuple(hdr1[3:9])
+                # Grab only data from input sensor and satellite
+                if sat_id == said and sensor_id == siid:                    
+                    # Loop through variables
+                    for var in self.variables.keys():
+                        if var == 'timestamp':
+                            mnemonic = ' '.join(self.variables[var]['datetime'].values())
+                            dt_hdr = bufr.read_subset(mnemonic).squeeze()
+                            data = '%04i%02i%02i%02i%02i' % tuple(dt_hdr)
+                        else:
+                            mnemonic = self.variables[var]['mnemonic']
+                            data = bufr.read_subset(mnemonic).squeeze()
 
-                    channels = bufr.read_subset('CHNM', rep=True).squeeze()  # channels
-                    tb = bufr.read_subset('TMBR', rep=True).squeeze()  # brightness temp
+                        for k, chan in enumerate(channels):
+                            if type(data) is list:
+                                df_dict[var].append(float(data[k]))
+                            elif var == 'channelNumber':
+                                df_dict[var].append(int(chan))
+                            else:
+                                df_dict[var].append(data)
+        bufr.close()
 
-                    for k, chan in enumerate(channels):
-                        df_dict['datetime'].append(str_datetime)
-                        df_dict['latitude'].append(hdr1[9])
-                        df_dict['longitude'].append(hdr1[10])
-                        df_dict['satellite_zenith_angle'].append(hdr2[0])
-                        df_dict['solar_zenith_angle'].append(hdr2[1])
-                        df_dict['bearing_or_azimuth'].append(hdr2[2])
-                        df_dict['solar_azimuth'].append(hdr2[3])
-                        df_dict['channel'].append(int(chan))
-                        df_dict['brightness temperature'].append(tb[k])
+        df = pd.DataFrame(df_dict)
+
+        return df
+    
+    def read_aircraft(self):
+        """
+        Open and read bufr file for aircraft data.
+        """
+
+        # Create output dictionary
+        df_dict = {}
+        for key in self.variables.keys():
+            df_dict[key] = []
+
+        # Open BUFR and store into dataframe
+        bufr = ncepbufr.open(self.bufr_file)
+        while bufr.advance() == 0:
+            while bufr.load_subset() == 0:
+                for var in self.variables.keys():
+                    if var == 'timestamp':
+                        mnemonic = ' '.join(self.variables[var]['datetime'].values())
+                        dt_hdr = bufr.read_subset(mnemonic).squeeze()
+                        data = '%04i%02i%02i%02i%02i' % tuple(dt_hdr)
+                    else:
+                        mnemonic = self.variables[var]['mnemonic']
+                        data = bufr.read_subset(mnemonic).squeeze()
+                        
+                    df_dict[var].append(data)
 
         bufr.close()
 
         df = pd.DataFrame(df_dict)
-        df.set_index('channel', inplace=True)
 
         return df
+
+    def _channel_list(self, input_chans):
+        """
+        Create of list of channels from a str input that could contain
+        a '-' or ',' characters
+
+        Args:
+            input_chans: (str) input string
+        """
+        chan_groups = [x.strip() for x in input_chans.split(',')]
+        channels = []
+
+        for c in chan_groups:
+            if '-' in c:
+                comps = c.split('-')
+                x = range(int(comps[0]), int(comps[1])+1)
+                channels.extend(x)
+            else:
+                channels.extend([int(c)])
+
+        return channels
