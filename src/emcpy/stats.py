8000d6f090076118be283fa3b17fc9cc1e73be4f
Andrew Collard <andrew.collard@noaa.gov>
2022-02-17 20:10:46 +0000

    GitHub Issue NOAA-EMC/emcpy#95: Fix pycodestyle issues

--- src/emcpy/stats.py
+++ src/emcpy/stats.py
@@ -290,7 +290,7 @@ def bootstrap(insample, level=.95, estimator='mean', nrepl=10000):
 
 def spectrum_stats(df):
     '''
-    Function to calculate major statistics for radiance data as a function of channel.  
+    Function to calculate major statistics for radiance data as a function of channel.
     Statisics are for observations that pass QC only.
 
     Args:
@@ -301,29 +301,27 @@ def spectrum_stats(df):
         channel_stats : (Pandas dataframe) A data frame containing channel-by-channel statistics
     '''
 
-    print('Calling Spectrum_Stats')
-
     # Get channel numbers in dataframe
     sc=df.index.unique(level="Channel")
 
     # Initialise output dataframe
-    channel_stats = _pd.DataFrame(index=sc,
+    channel_stats = _pd.DataFrame(index = sc,
         columns = ["count","omf_unadjusted_mean", "omf_adjusted_mean",
-            "omf_unadjusted_stddev", "omf_adjusted_stddev"])
+                   "omf_unadjusted_stddev", "omf_adjusted_stddev"])
 
     # Sorting by channel increases efficiency and prevents a warning message
-    dfs=df.sort_index()
+    dfs = df.sort_index()
 
     # Loop through channels and calculate stats
     for chan in sc:
         # Subset based on channel and QC indexes
-        tmp = dfs.loc[(chan,0.0)]
+        tmp = dfs.loc[(chan, 0.0)]
         # This next line makes sure that all rejected obs are avoided.
         # Using the inverse observation error is more reliable than the QC flag.
         tmp = tmp[(tmp['inverse_observation_error'] > 0.0)]
         channel_stats["count"].loc[chan] = tmp.size
         for var in ["omf_unadjusted", "omf_adjusted"]:
-            channel_stats[var+'_mean'].loc[chan]=tmp[var].to_numpy().mean()
-            channel_stats[var+'_stddev'].loc[chan]=tmp[var].to_numpy().std()
+            channel_stats[var + '_mean'].loc[chan] = tmp[var].to_numpy().mean()
+            channel_stats[var + '_stddev'].loc[chan] = tmp[var].to_numpy().std()
 
     return channel_stats
