806156241ca59a1b857450cce6be26cf02a9cac5
Kevin Dougherty <kevin.dougherty@noaa.gov>
2022-09-09 16:01:59 +0000

    change setup.py script

--- setup.py
+++ setup.py
@@ -1,2 +1,46 @@
 import setuptools
-setuptools.setup()
+setuptools.setup(
+    name = 'emcpy',
+    version = '0.0.1',
+    description = 'A collection of python tools used at EMC',
+    author = 'NOAA-EMC',
+    author_email = 'rahul.mahajan@noaa.gov',
+    url = 'https://github.com/noaa-emc/emcpy',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    classifiers = [
+        'Development Status :: 1 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Lesser General Public License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: OS Independent',
+        'Typing :: Typed'],
+    project_urls = [
+        Bug Tracker = 'https://github.com/noaa-emc/emcpy/issues',
+        CI = 'https://github.com/noaa-emc/emcpy/actions'],
+    python_requires='>=3.6',
+    install_requires=[
+        'pyyaml>=6.0',
+        'pycodestyle>=2.8.0',
+        'netCDF4>=1.5.3',
+        'matplotlib>=3.5.2',
+        'cartopy>=0.20.2',
+        'scikit-learn>=1.0.2',
+        'xarray>=0.11.3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'eva = eva.eva_base:main',
+            'eva_tests = eva.eva_tests:main',
+        ],
+    },
+)
