86cf7a8d193d19950a6c2013a7fd707082b50e08
donald lippi <donald.e.lippi@clogin05.cactus.wcoss2.ncep.noaa.gov>
2022-12-28 16:35:07 +0000

    added filter_obs function which creates a bool array used to filter by. Considers obs filtered by the use flag,max/min pressure, latitude, longitude, and errors along with filtering by bufr code.

--- src/emcpy/io/netCDF.py
+++ src/emcpy/io/netCDF.py
@@ -9,6 +9,19 @@ __all__ = ['variable_exist', 'read_netCDF_var']
 import numpy as _np
 from netCDF4 import Dataset as _Dataset
 
+def print_variables(filename):
+    '''
+    prints all available variables in a netCDF file 
+
+    Args:
+        filename : (str) netCDF filename
+    '''
+    try:
+        nc = _Dataset(filename, 'r')
+    except IOError:
+        raise IOError(f'Unable to open {filename}')
+    print(nc.variables.keys())
+
 
 def variable_exist(filename, variable_name):
     '''
@@ -79,3 +92,47 @@ def read_netCDF_var(filename, variable_name, oneD=False, ftime=-1, flevel=-1):
 
     result = _np.squeeze(var)
     return result
+
+def filter_obs(code,codes,errorinv,lat,lon,pressure,\
+               use=1,p_max=1050.0,p_min=100.0,lat_max=90.0,lat_min=0.0,\
+               lon_max=360.0,lon_min=0.0,error_max=40.0,error_min=0.000001):
+    """
+    Create the bool array used to filter by. Considers obs filtered by the use flag,
+    max/min pressure, latitude, longitude, and errors along with filtering by bufr
+    code (e.g., 187 or 287).
+
+    netCDF Args:
+        code     : (array int) bufr ob codes from netCDF file <Observation_Type>
+        codes    : (array int) bufr ob codes to filter by, see link 
+                   https://emc.ncep.noaa.gov/mmb/data_processing/prepbufr.doc/table_2.htm
+        errorinv : (array float) from netCDF file <Errinv_Final>
+        lat      : (array float) from netCDF file <Latitude>
+        lon      : (array float) from netCDF file <Longitude>
+        pressure : (array float) from netCDF file <Pressure>
+
+     Filtering Args:
+        use      : (int)   use flag for observation 1=used
+        p_max    : (float) maximum pressure (mb) for including observation in calculations
+        p_min    : (float) minimum pressure (mb) for including observation in calculations
+        lat_max  : (float) maximum latitude (deg N) for including observation in calculations
+        lat_min  : (float) minimum latitude (deg N) for including observation in calculations
+        lon_max  : (float) maximum latitude (deg E) for including observation in calculations
+        lon_min  : (float) minimum latitude (deg E) for including observation in calculations
+        error_max: (float) maximum error standard deviation for including observation in calculations
+        error_min: (float) minimum error standard deviation for including observation in calculations
+
+    Returns:
+        used : (array bool) observations to consider after all filtering
+    """
+    used = code==codes[0] #initialize used
+    for cd in codes:
+        used = np.logical_or(used,code==cd) #loop over all codes provided
+    
+    #consider where use flag==1 and bound by error/lat/lon/pres 
+    used = np.logical_and(used,use==1)
+    used = np.logical_and(used,np.logical_and(errorinv > error_min,errorinv < error_max))
+    used = np.logical_and(used,np.logical_and(lat>=lat_min,lat<=lat_max))
+    used = np.logical_and(used,np.logical_and(lon>=lon_min,lon<=lon_max))
+    used = np.logical_and(used,np.logical_and(pressure>=p_min,pressure<=p_max))
+
+    return used
