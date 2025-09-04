185e57567a29884de8662fe59eefbd6e992cb756
kevindougherty-noaa <Kevin.Dougherty@noaa.gov>
2025-09-04 14:41:17 +0000

    fix fixture

--- src/tests/plotting/test_ticks.py
+++ src/tests/plotting/test_ticks.py
@@ -8,13 +8,13 @@ from emcpy.plots.plots import LinePlot
 from emcpy.plots.create_plots import CreatePlot, CreateFigure
 
 
-def test_datetime_xticks_replace_autos():
+def test_datetime_xticks_replace_autos(single_axes):
     lp = LinePlot([0, 1, 2, 3, 4, 5], [1, 1, 2, 3, 5, 8])
     start = datetime(2025, 9, 2, 0, 0)
     ticks = [start + timedelta(hours=h) for h in range(6)]
     plot = CreatePlot(plot_layers=[lp])
     plot.set_xticks(ticks=ticks, date_format="%H:%M")
-    _, ax = _single_axes(plot)
+    _, ax = single_axes(plot)
     assert len(ax.get_xticks()) == len(ticks)
     assert isinstance(ax.xaxis.get_major_formatter(), mdates.DateFormatter)
 
@@ -41,7 +41,7 @@ def test_minor_labels_forbidden():
         fig.create_figure()
 
 
-def test_yaxis_date_format_is_supported():
+def test_yaxis_date_format_is_supported(single_axes):
     lp = LinePlot([0, 1, 2, 3], [0, 1, 2, 3])
     start = datetime(2025, 9, 2, 0, 0)
     yticks = [start + timedelta(hours=h) for h in range(4)]
@@ -52,10 +52,10 @@ def test_yaxis_date_format_is_supported():
     assert isinstance(ax.yaxis.get_major_formatter(), mdates.DateFormatter)
 
 
-def test_setting_major_ticks_clears_minor_locator_by_default():
+def test_setting_major_ticks_clears_minor_locator_by_default(single_axes):
     lp = LinePlot([0, 1, 2], [0, 1, 2])
     plot = CreatePlot(plot_layers=[lp])
     plot.set_xticks(ticks=[0.5, 1.5], minor=True)
     plot.set_xticks(ticks=[0, 1, 2])  # should clear minor
-    _, ax = _single_axes(plot)
+    _, ax = single_axes(plot)
     assert isinstance(ax.xaxis.get_minor_locator(), NullLocator)
