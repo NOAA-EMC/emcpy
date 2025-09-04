4ef14614f5f72cbb270a4311c8e2df79f16aacf2
kevindougherty-noaa <Kevin.Dougherty@noaa.gov>
2025-09-04 21:09:47 +0000

    remove unnecessary test

--- src/tests/plotting/test_layers_basic.py
+++ src/tests/plotting/test_layers_basic.py
@@ -247,16 +247,6 @@ def test_box_and_whisker_orientation_horizontal(single_axes):
     assert ax.artists or ax.lines or ax.patches or ax.collections
 
 
-def test_box_and_whisker_vert_property_sets_orientation(recwarn):
-    data = [[1, 2, 3], [2, 3, 4]]
-    b = BoxandWhiskerPlot(data)
-    b.vert = False  # deprecated pathway
-    # Should warn exactly once, and update orientation
-    w = recwarn.pop(DeprecationWarning)
-    assert "BoxandWhiskerPlot.vert is deprecated" in str(w.message)
-    assert b.orientation == 'horizontal'
-
-
 def test_horizontal_span(single_axes):
     levs = np.linspace(975, 125, 23)
     rms = [1.8, 2.02, 2.36, 2.10, 2.21, 2.17, 2.08, 2.14, 2.14, 2.19,
