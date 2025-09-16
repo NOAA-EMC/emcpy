87d7848113268d9793b835d9efd56ecf5003c40d
kevindougherty-noaa <Kevin.Dougherty@noaa.gov>
2025-09-16 20:34:56 +0000

    pycodestyle

--- src/tests/plotting/test_integer_field_norms.py
+++ src/tests/plotting/test_integer_field_norms.py
@@ -2,13 +2,19 @@ import numpy as np
 from emcpy.plots.create_plots import CreatePlot, CreateFigure
 from emcpy.plots.map_plots import MapGridded
 
+
 def test_integer_field_requires_bounds_or_levels():
-    lat = np.linspace(-10, 10, 5); lon = np.linspace(0, 20, 5)
+    lat = np.linspace(-10, 10, 5)
+    lon = np.linspace(0, 20, 5)
     LON, LAT = np.meshgrid(lon, lat)
-    Z = np.ones((5,5), dtype=int) * 3
-    mg = MapGridded(LAT, LON, Z); mg.integer_field = True
+    Z = np.ones((5, 5), dtype=int) * 3
+    mg = MapGridded(LAT, LON, Z)
+    mg.integer_field = True
     p = CreatePlot(projection="plcarr", domain="global", plot_layers=[mg])
-    fig = CreateFigure(1,1); fig.plot_list=[p]
+    fig = CreateFigure(1, 1)
+    fig.plot_list = [p]
     # supply vmin/vmax to avoid ValueError
-    mg.vmin = 0; mg.vmax = 5
-    fig.create_figure(); fig.close_figure()
+    mg.vmin = 0
+    mg.vmax = 5
+    fig.create_figure()
+    fig.close_figure()
